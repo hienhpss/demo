e9875a541ebd3c881d62b2806b5bb5ba7dd9ef51
hienhpss <thanhhien008@gmail.com>
2016-11-05 17:43:46 +0800

    use delegate

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
 	validates_presence_of :name
+	delegate :name, :to => :category, :prefix => true,
+ :allow_nil => true
 	has_many :attendees # plural
 	has_many :event_groupships # plural
  	has_many :groups, :through => :event_groupships
